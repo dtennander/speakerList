434f3bfa69e023c0e1d8cb87bdcb71568bd6fdf8
David Tennander <david.tennander@gmail.com>
2018-10-19 22:39:41 +0200

    Adds closing of share screen on click outside.

--- client/src/components/ShareScreen/index.tsx
+++ client/src/components/ShareScreen/index.tsx
@@ -1,21 +1,61 @@
 import * as React from "react";
-import {Ref} from "react";
+import {PureComponent, RefObject} from "react";
 import * as styles from "./styles.css";
 
-const splashScreenInput : Ref<HTMLInputElement> = React.createRef();
 
-const ShareScreen = (props : {url: string, onClose:()=> void}) => (
-    <div className={styles.spashScreen}>
-        <button onClick={props.onClose} className={styles.exitButton}>x</button>
-        <p>Share this list by sending this link:</p>
-        <input ref={splashScreenInput} onSelect={(event) => event.currentTarget.select()} readOnly={true} value={props.url}/>
-        <button onClick={(event) => handleClickOnCopyButton()}>📋</button>
-    </div>
-);
+interface ShareScreenProps {
+    url: string,
+    onClose:()=> void
+}
 
-const handleClickOnCopyButton = () => {
-    splashScreenInput.current.select();
-    document.execCommand('copy');
-};
+
+class ShareScreen extends PureComponent<ShareScreenProps> {
+
+    splashScreenInput : RefObject<HTMLInputElement>;
+
+    containerRef: RefObject<HTMLDivElement>;
+
+    constructor(props : ShareScreenProps) {
+        super(props);
+        this.splashScreenInput = React.createRef();
+        this.containerRef = React.createRef();
+    }
+
+    componentDidMount(): void {
+        document.addEventListener(
+            "mousedown", (event) => this.handleClickAnyWare(event))
+    }
+
+    componentWillUnmount(): void {
+        document.removeEventListener(
+            "mousedown", (event) => this.handleClickAnyWare(event))
+    }
+
+    render() {
+        return (
+            <div ref={this.containerRef} className={styles.spashScreen}>
+                <button onClick={this.props.onClose} className={styles.exitButton}>x</button>
+                <p>Share this list by sending this link:</p>
+                <input ref={this.splashScreenInput}
+                       onSelect={(event) => event.currentTarget.select()}
+                       readOnly={true}
+                       value={this.props.url}/>
+                <button onClick={() => this.handleClickOnCopyButton()}>📋</button>
+            </div>
+        );
+    }
+
+    handleClickOnCopyButton = () => {
+        this.splashScreenInput.current.select();
+        document.execCommand('copy');
+    };
+
+    handleClickAnyWare = (event : MouseEvent) => {
+        if (this.containerRef.current && !this.containerRef.current.contains((event.currentTarget as Node))) {
+           this.props.onClose()
+        }
+    }
+
+}
 
 export default ShareScreen
