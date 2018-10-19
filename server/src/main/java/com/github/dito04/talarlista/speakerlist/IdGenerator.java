0d57a4a2256dd4a074a74f9c1bed8a6ac6b36cb7
David Tennander <david.tennander@gmail.com>
2018-10-19 23:34:09 +0200

    Updates seassion ids to base64 strings.

--- server/src/main/java/com/github/dito04/talarlista/speakerlist/IdGenerator.java
+++ server/src/main/java/com/github/dito04/talarlista/speakerlist/IdGenerator.java
@@ -1,17 +1,22 @@
 package com.github.dito04.talarlista.speakerlist;
 
-import java.util.UUID;
+import java.time.Instant;
+import java.util.Base64;
 
 /**
  * Generates IDs
  */
 class IdGenerator {
+  private static final long TIME_OFFSET = 1_500_000_000L;
+  private Base64.Encoder encoder = Base64.getEncoder();
 
   /**
    * Generates a unique id.
    * @return the generated id.
    */
   String generateId() {
-    return UUID.randomUUID().toString();
+    Instant now = Instant.now();
+    long seed = now.getEpochSecond() + now.getNano() - TIME_OFFSET;
+    return new String(encoder.encode(Long.toString(seed).getBytes()));
   }
 }
